cdd3b65c03986449654e1dc4af14bfde55f6a7ac
kevinysr20 <kevinysramalho@gmail.com>
2024-11-17 15:26:33 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,23 @@
 criaCartao(
     'futebol',
     'Quem é o melhor jogador do mundo?',
-    'O melhor jogador do mundo é Cristiano Ronaldo'
+    'O melhor jogador do mundo é Cristiano Ronaldo.'
 )
 criaCartao(
     'Geografia',
-    'Qual a capital de Portugal',
-    'A capital de Portugal é Lisboa'
+    'Qual a capital de Portugal?',
+    'A capital de Portugal é Lisboa.'
 )
 
 criaCartao(
     'automovél',
     'Qual é o carro mais rápido do mundo?',
-    'O carro mais rapido do mundo é o Koenigsegg Agera RS'
+    'O carro mais rapido do mundo é o Koenigsegg Agera RS.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz fantasma em inglês?',
-    'È ghost'
+    'Como se diz futebol em inglês?',
+    'A palavra futebol em inglês é 
+soccer.'
 )
